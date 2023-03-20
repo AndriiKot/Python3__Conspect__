9a9c4f20428ea3f899033cba9e250e8c582fb16e
AndriiKot <a_kocyuba@ukr.net>
2023-03-20 23:16:34 +0100

    Add variable full_name

--- Lesson1.rb
+++ Lesson1.rb
@@ -31,6 +31,47 @@ p arr                 #[1,"2",[],{}]
 arr.map!(&:class)
 p arr                 # !!! [Integer,String,Array,Hash]
 
+=begin 
+JS
+let my_first_name
+let my_last_name
+let full_name
+
+my_first_name = "Andrii"
+my_last_name = 'Kotsiuba'
+full_name = my_first_name + ' ' + my_last_name 
+
+console.log(full_name)                          // Andrii Kotsiuba
+=end
+my_first_name,my_last_name = 'Andrii','Kotsiuba'
+full_name = my_first_name + ' ' + my_last_name
+p full_name              # "Andrii Kotsiuba"
+
+=begin
+
+console.log(9/5)  // 1.8  !!!!!!!
+
+let i = 5
+let x = 8
+console.log(i++)  // 5  !!!!!!!!!
+console.log(i)    // 6   
+console.log(++x)  // 9
+console.log(x)    // 9 
+
+let z = 6
+let y = 3 
+console.log(z--) // 6  !!!!!!!
+console.log(z)   // 5
+
+console.log(--y) // 2
+console.log(y)   // 2
+
+let a = 1
+let b = 1
+b += 1
+a = a + 1
+=end
+
 
 
 
